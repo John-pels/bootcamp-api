5ca2136bfc9ee52b5823ea18e2fc1f3f49c62902
John O. Ajeigbe <ajeigbejohnolu@gmail.com>
2024-05-01 10:17:52 +0100

    ci: add morgan loggin middleware

---
+++ middleware/logger.js
@@ -0,0 +1,10 @@
+// @desc Logs requests to the console
+
+const logger = (req, res, next) => {
+  console.log(
+    `${req.method} ${req.protocol}://${req.get("host")}${req.originalUrl}`
+  );
+  next();
+};
+
+module.exports = logger;
--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const bootcampsRouter = require("./routes/bootcamps");
-
+const morgan = require("morgan");
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 app.use("/api/v1/bootcamps", bootcampsRouter);
 
 const PORT = process.env.PORT || 8000;
