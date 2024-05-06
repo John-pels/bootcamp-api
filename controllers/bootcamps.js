93e41362d9033a18ab2d5e92dd722e96978b70b2
John O. Ajeigbe <ajeigbejohnolu@gmail.com>
2024-05-06 22:41:06 +0100

    chore: add an endpoint to get bootcamps within a radius with zipcode and distance

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,6 +1,7 @@
 const Bootcamp = require("../models/Bootcamp");
 const asyncHandler = require("../middleware/async");
 const ErrorResponse = require("../utils/errorResponse");
+const geocoder = require("../utils/geocoder");
 
 // @desc   Get all bootcamps
 // @route   GET /api/v1/bootcamps
@@ -47,7 +48,6 @@ exports.updateBootcamp = asyncHandler(async (req, res, next) => {
       new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
     );
   }
-
   res.status(200).json({ success: true, data: bootcamp });
 });
 
@@ -56,12 +56,37 @@ exports.updateBootcamp = asyncHandler(async (req, res, next) => {
 // @access  Private
 exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
   const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
-
   if (!bootcamp) {
     return next(
       new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
     );
   }
-
   res.status(200).json({ success: true, data: {} });
+});
+
+// @desc   Get bootcamps within a radius
+// @route   DELETE /api/v1/bootcamps/radius/:zipcode/:distance
+// @access  Private
+exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
+  const { zipcode, distance } = req.params;
+  // Get latitude and longitude from geocoder
+  const loc = await geocoder.geocode(zipcode);
+  const latitude = loc[0].latitude;
+  const longitude = loc[0].longitude;
+
+  //Calculate the radius using radians
+  //Divide distance by radius of the Earth
+  //Earth Radiius = 3,963 mi / 6,378 km
+  const radius = distance / 3963;
+  const bootcamps = await Bootcamp.find({
+    location: {
+      $geoWithin: { $centerSphere: [[longitude, latitude], radius] },
+    },
+  });
+
+  res.status(200).json({
+    success: true,
+    count: bootcamps.length,
+    data: bootcamps,
+  });
 });
--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -5,9 +5,12 @@ const {
   createBootcamp,
   updateBootcamp,
   deleteBootcamp,
+  getBootcampsInRadius,
 } = require("../controllers/bootcamps");
 
 const bootcampsRouter = express.Router();
+
+bootcampsRouter.route("/radius/:zipcode/:distance").get(getBootcampsInRadius);
 
 bootcampsRouter.route("/").get(getBootcamps).post(createBootcamp);
 
