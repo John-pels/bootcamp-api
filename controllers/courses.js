4a403fd3bc0f8a69dabd1e5c7843201787bf2cbb
John O. Ajeigbe <ajeigbejohnolu@gmail.com>
2024-05-12 18:44:08 +0100

    chore: add controllers and routes to update and delete a course

--- controllers/courses.js
+++ controllers/courses.js
@@ -69,3 +69,48 @@ exports.addCourse = asyncHandler(async (req, res, next) => {
     data: course,
   });
 });
+
+// @desc    Update a course
+// @route   PUT /api/v1/courses/:id
+// @access  Private
+exports.updateCourse = asyncHandler(async (req, res, next) => {
+  let course = await Course.findById(req.params.id);
+
+  if (!course) {
+    return next(
+      new ErrorResponse(`No course with the id of ${req.params.id}`),
+      404
+    );
+  }
+
+  course = await Course.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    data: course,
+  });
+});
+
+// @desc    Delete a course
+// @route   DELETE /api/v1/courses/:id
+// @access  Private
+exports.deleteCourse = asyncHandler(async (req, res, next) => {
+  const course = await Course.findById(req.params.id);
+
+  if (!course) {
+    return next(
+      new ErrorResponse(`No course with the id of ${req.params.id}`),
+      404
+    );
+  }
+
+  await course.deleteOne();
+
+  res.status(200).json({
+    success: true,
+    data: {},
+  });
+});
--- routes/courses.js
+++ routes/courses.js
@@ -1,9 +1,19 @@
 const express = require("express");
-const { getCourses, getCourse, addCourse } = require("../controllers/courses");
+const {
+  getCourses,
+  getCourse,
+  addCourse,
+  updateCourse,
+  deleteCourse,
+} = require("../controllers/courses");
 
 const coursesRouter = express.Router({ mergeParams: true });
 
 coursesRouter.route("/").get(getCourses).post(addCourse);
-coursesRouter.route("/:id").get(getCourse);
+coursesRouter
+  .route("/:id")
+  .get(getCourse)
+  .put(updateCourse)
+  .delete(deleteCourse);
 
 module.exports = coursesRouter;
