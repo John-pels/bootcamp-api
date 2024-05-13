69d74251c1669526c6f52726dec4e1f09e514c68
John O. Ajeigbe <ajeigbejohnolu@gmail.com>
2024-05-13 13:31:49 +0100

    chore: add user model and password encryption middleware

---
+++ models/User.js
@@ -0,0 +1,43 @@
+const bcrypt = require("bcryptjs");
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please add a name"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please add an email address"],
+    unique: true,
+    match: [
+      /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/,
+      "Please add a valid email address",
+    ],
+  },
+  role: {
+    type: String,
+    enum: ["user", "publisher"],
+    default: "user",
+  },
+  password: {
+    type: String,
+    required: [true, "Please add a password"],
+    minLength: 6,
+    select: false,
+  },
+  resetPasswordToken: String,
+  resetPasswordExpire: Date,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// Encrypt the password using bcrypt
+UserSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+module.exports = mongoose.model("User", UserSchema);
