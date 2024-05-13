79e7117697677bcb8f22008da752a492de267895
John O. Ajeigbe <ajeigbejohnolu@gmail.com>
2024-05-13 13:32:56 +0100

    chore: mount the auth router

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const path = require("path");
 const morgan = require("morgan");
 const express = require("express");
 const connectDB = require("./config/db");
+const authRouter = require("./routes/auth");
 const fileupload = require("express-fileupload");
 const errorHandler = require("./middleware/error");
 const coursesRouter = require("./routes/courses");
@@ -31,6 +32,7 @@ app.use(express.static(path.join(__dirname, "public")));
 //Mount routers
 app.use("/api/v1/bootcamps", bootcampsRouter);
 app.use("/api/v1/courses", coursesRouter);
+app.use("/api/v1/auth", authRouter);
 
 app.use(errorHandler);
 
