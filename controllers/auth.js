e3282c55714d3fa455ec9235733cc3b61ea1e917
John O. Ajeigbe <ajeigbejohnolu@gmail.com>
2024-05-13 13:33:52 +0100

    chore: create a controller for the register route

---
+++ controllers/auth.js
@@ -0,0 +1,22 @@
+const User = require("../models/User");
+const asyncHandler = require("../middleware/async");
+const ErrorResponse = require("../utils/errorResponse");
+
+// @desc   Register a user
+// @route  GET /api/v1/auth/register
+// @access Public
+exports.register = asyncHandler(async (req, res, next) => {
+  const { name, email, password, role } = req.body;
+
+  // Create a new user
+  const user = await User.create({
+    name,
+    email,
+    password,
+    role,
+  });
+
+  res.status(200).json({
+    success: true,
+  });
+});
