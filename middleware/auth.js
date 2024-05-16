310b3c9cab834fb238bf0aafe8758653109a8362
John O. Ajeigbe <ajeigbejohnolu@gmail.com>
2024-05-16 04:17:32 +0100

    chore: implement logout to clear token

--- middleware/auth.js
+++ middleware/auth.js
@@ -10,11 +10,13 @@ exports.protect = asyncHandler(async (req, res, next) => {
     req.headers.authorization &&
     req.headers.authorization.startsWith("Bearer")
   ) {
+    // Set token from Authorization: Bearer token
     token = req.headers.authorization.split(" ")[1];
   }
-  //   else if (req.cookies.token) {
-  //       token = req.cookies.token
-  //     }
+  // // Set token from cookie
+  // else if (req.cookies.token) {
+  //   token = req.cookies.token;
+  // }
 
   // Make sure token exists
   if (!token) {
--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const express = require("express");
 const {
   login,
   getMe,
+  logout,
   register,
   resetPassword,
   updateDetails,
@@ -13,6 +14,7 @@ const authRouter = express.Router();
 
 authRouter.post("/register", register);
 authRouter.post("/login", login);
+authRouter.get("/logout", logout);
 authRouter.get("/me", protect, getMe);
 authRouter.put("/update-details", protect, updateDetails);
 authRouter.put("/update-password", protect, updatePassword);
